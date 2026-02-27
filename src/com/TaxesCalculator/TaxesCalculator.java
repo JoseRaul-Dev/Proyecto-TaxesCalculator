de4082d360a6818626a64d37c1981028092fd43e
Ivan mena damian <ivanmenadamian2002@gmail.com>
2026-02-27 19:50:54 +0100

    Segunda refactorizacion

--- src/com/TaxesCalculator/TaxesCalculator.java
+++ src/com/TaxesCalculator/TaxesCalculator.java
@@ -1,23 +1,33 @@
 package com.TaxesCalculator;
 
 public class TaxesCalculator {
-
+	private static final double Descuento_Casado = 0.95;
+	private static final double Ratio_Alto = 0.25;
+	private static final double Ratio_Medio = 0.15;
+	private static final double Ratio_Bajo = 0.10;
+	private static final int Base_Alta = 6500;
+	private static final int Tasa_Maxima = 50000;
+	private static final int Tasa_Minima = 20000;
+	private static final int Base_Media = 2000;
+	
+	
     public double calcularImpuesto(double ingreso, String categoria, boolean casado) {
         double impuesto = 0;
         if (categoria.equals("A")) {
-            if (ingreso < 20000) {
-                impuesto = ingreso * 0.10;
-            } else if (ingreso < 50000) {
-                impuesto = 2000 + (ingreso - 20000) * 0.15;
+           
+			if (ingreso < Tasa_Minima) {
+                impuesto = ingreso * Ratio_Bajo;
+            } else if (ingreso < Tasa_Maxima) {
+                impuesto = Base_Media + (ingreso - Tasa_Minima) * Ratio_Medio;
             } else {
-                impuesto = 6500 + (ingreso - 50000) * 0.25;
+                impuesto = Base_Alta + (ingreso - 50000) * Ratio_Alto;
             }
         } else if (categoria.equals("B")) {
             impuesto = ingreso * 0.20;
         }
         
         if (casado) {
-            impuesto = impuesto * 0.95;
+            impuesto = impuesto * Descuento_Casado;
         }
         
         return impuesto;
