e1cc981132e9387377b9f6ae35175eafa9b4ad2c
Ivan mena damian <ivanmenadamian2002@gmail.com>
2026-02-27 19:34:58 +0100

    Segunda Refactorizacion

--- src/com/TaxesCalculator/TaxesCalculator.java
+++ src/com/TaxesCalculator/TaxesCalculator.java
@@ -2,22 +2,27 @@ package com.TaxesCalculator;
 
 public class TaxesCalculator {
 
-    public double calcularImpuesto(double ingreso, String categoria, boolean casado) {
+    private static final double Descuento_casado = 0.95;
+	private static final double Ratio_Alto = 0.25;
+	private static final double Ratio_Medio = 0.15;
+	private static final double Ratio_Bajo = 0.10;
+
+	public double calcularImpuesto(double ingreso, String categoria, boolean casado) {
         double impuesto = 0;
         if (categoria.equals("A")) {
             if (ingreso < 20000) {
-                impuesto = ingreso * 0.10;
+                impuesto = ingreso * Ratio_Bajo;
             } else if (ingreso < 50000) {
-                impuesto = 2000 + (ingreso - 20000) * 0.15;
+                impuesto = 2000 + (ingreso - 20000) * Ratio_Medio;
             } else {
-                impuesto = 6500 + (ingreso - 50000) * 0.25;
+                impuesto = 6500 + (ingreso - 50000) * Ratio_Alto;
             }
         } else if (categoria.equals("B")) {
             impuesto = ingreso * 0.20;
         }
         
         if (casado) {
-            impuesto = impuesto * 0.95;
+            impuesto = impuesto * Descuento_casado;
         }
         
         return impuesto;
